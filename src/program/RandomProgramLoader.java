ebc42c621bc103b4e3f20063ccd8b8ca335ffe66
mdco-ld <migueldco14@gmail.com>
2024-06-22 22:57:13 -0300

    it works

---
+++ src/program/RandomProgramLoader.java
@@ -0,0 +1,69 @@
+package program;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class RandomProgramLoader implements ProgramLoader {
+    @Override
+    public Program loadProgram(String fileName) {
+        if (fileName.startsWith("IO")) {
+            int num = Integer.parseInt(fileName.substring(2));
+            return randomIOBoundProgram(num);
+        } else if (fileName.startsWith("CPU")) {
+            int num = Integer.parseInt(fileName.substring(3));
+            return randomCPUBoundProgram(num);
+        }
+        int num = Integer.parseInt(fileName);
+        return randomProgram(num);
+    }
+
+    private Program randomIOBoundProgram(int size) {
+        Random rng = new Random();
+        List<Instruction> instructions = new ArrayList<>();
+        int numBlock = Math.abs(rng.nextInt() % size);
+        if (numBlock < size - numBlock) {
+            numBlock = size - numBlock;
+        }
+        for (int i=0;i<numBlock;i++) {
+            int duration = Math.abs(rng.nextInt() % 10);
+            instructions.add(new BlockInstruction(duration));
+        }
+        for (int i=numBlock;i<size;i++) {
+            instructions.add(new ExecuteInstruction());
+        }
+        Collections.shuffle(instructions);
+        return new Program(instructions, "IO");
+    }
+    private Program randomCPUBoundProgram(int size) {
+        Random rng = new Random();
+        List<Instruction> instructions = new ArrayList<>();
+        int numExecute = Math.abs(rng.nextInt() % size);
+        if (numExecute < size - numExecute) {
+            numExecute = size - numExecute;
+        }
+        for (int i=0;i<numExecute;i++) {
+            instructions.add(new ExecuteInstruction());
+        }
+        for (int i=numExecute;i<size;i++) {
+            int duration = Math.abs(rng.nextInt() % 10);
+            instructions.add(new BlockInstruction(duration));
+        }
+        Collections.shuffle(instructions);
+        return new Program(instructions, "CPU");
+    }
+    private Program randomProgram(int size) {
+        Random rng = new Random();
+        List<Instruction> instructions = new ArrayList<>();
+        for (int i=0;i<size;i++) {
+            if (rng.nextBoolean()) {
+                instructions.add(new ExecuteInstruction());
+            } else {
+                int duration = Math.abs(rng.nextInt() % 10);
+                instructions.add(new BlockInstruction(duration));
+            }
+        }
+        return new Program(instructions, "Random");
+    }
+}
---
+++ src/simulation/SimulatedProcess.java
@@ -0,0 +1,48 @@
+package simulation;
+
+import program.BlockInstruction;
+import program.Program;
+
+public class SimulatedProcess {
+    private final Program program;
+    private final long processId;
+    private int currentInstruction;
+    private int blockTime;
+    SimulatedProcess(Program program, long processId) {
+        this.program = program;
+        currentInstruction = 0;
+        this.processId = processId;
+        blockTime = 0;
+    }
+
+    public String getName() {
+        return program.getFilename();
+    }
+
+    public long getPid() {
+        return processId;
+    }
+
+    public void executeInstruction(int quantum) {
+        System.out.printf("[%d] %s: Executing\n", getPid(), getName());
+        try {
+            Thread.sleep(quantum);
+        } catch (Exception e) {
+            System.err.println("Eu quero dormir!");
+        }
+        if (program.getBody().get(currentInstruction) instanceof BlockInstruction blockInstruction) {
+            System.out.printf("[%d] %s: Blocking for %d\n", getPid(), getName(), blockInstruction.getDuration());
+            blockTime = blockInstruction.getDuration();
+        }
+        currentInstruction++;
+    }
+    public boolean isDone() {
+        return currentInstruction >= program.getBody().size();
+    }
+    public boolean isBlocked() {
+        return blockTime > 0;
+    }
+    public void decrementBlockTime() {
+        blockTime--;
+    }
+}
